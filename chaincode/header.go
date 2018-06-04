8c445360131098b99e329fbb3251ea562150040c
Pierre Cluchet <p.cluchet@gmail.com>
2018-06-04 14:14:52 +0200

    headers for userInfos

--- chaincode/header.go
+++ chaincode/header.go
@@ -1,5 +1,16 @@
 package main
 
-type SimpleAsset	struct {
-		
+type SimpleAsset struct {
+}
+
+type AllowanceCouple struct {
+	Spender string
+	Amount  uint64
+}
+
+type AllowanceCouples []AllowanceCouple
+
+type UserInfos struct {
+	Amount     uint64
+	Allowances AllowanceCouples
 }
