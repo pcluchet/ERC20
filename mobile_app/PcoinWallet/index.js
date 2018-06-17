f26d9e4bf479c76f9d54c82f716275c9ce21b911
Pierre Cluchet <p.cluchet@gmail.com>
2018-06-17 17:35:34 +0200

    balance/transfer

--- mobile_app/PcoinWallet/index.js
+++ mobile_app/PcoinWallet/index.js
@@ -5,14 +5,28 @@ import {
     ActivityIndicator, // import des composants
     TouchableOpacity,
     Text,
+    TextInput,
     View,
 } from 'react-native'
 
 
-export const getMoviesFromApiAsync = () => {
+const APIURL = "http://192.168.0.2:8000";
+
+export const getUserBalance = (username) => {
     //return "hi";
 
-    return fetch('http://192.168.0.11/erc20/index.php')
+    return fetch(`${APIURL}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Transaction: 'balanceOf',
+          Id: username,
+          TokenOwner: username,
+        }),
+        })
         .then((response) => response.json())
         .then((responseJson) => {
 
@@ -29,10 +43,23 @@ export const getMoviesFromApiAsync = () => {
 
 }
 
-export const getUserList = () => {
+export const TransferTokens = (username, to, amount) => {
     //return "hi";
 
-    return fetch('http://192.168.0.11/erc20/userlist.php')
+
+    return fetch(`${APIURL}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Transaction: 'transfer',
+          Id: username,
+          To: to,
+          Tokens: amount,
+        }),
+        })
         .then((response) => response.json())
         .then((responseJson) => {
 
@@ -48,6 +75,7 @@ export const getUserList = () => {
         });
 
 }
+
 
 
 
@@ -59,11 +87,12 @@ class App extends Component {
     constructor(props) {
         super(props)
 
-        // la state de notre composant est utilisé pour
-        // stocker quelques infos renvoyées par l'API
         this.state = {
             name: '', // nom de la bière
+            username: '', // nom de la bière
             balance: '0', // nom de la bière
+            transferamount: '0', // nom de la bière
+            transferto: '', // nom de la bière
             description: '', // sa description
             BalanceIsLoading: false, // la requête API est-elle en cours ?
             UserListIsLoading: false // la requête API est-elle en cours ?
@@ -71,13 +100,19 @@ class App extends Component {
     }
 
 
+    ft_balanceOfSafe = (json) => {
+        if (json.result == 200)
+            return json.body;
+        else
+            return "0"
+    }
 
 
     ft_get = () => {
         this.setState({ BalanceisLoading: true })
         this.setState({ name: "blah" })
-        getMoviesFromApiAsync().then(json => this.setState({
-            balance: json.name,
+        getUserBalance(this.state.username).then(json => this.setState({
+            balance: this.ft_balanceOfSafe(json),
             BalanceisLoading: false // la requête est terminée
         }))
             .catch(error => console.error(error))
@@ -87,38 +122,104 @@ class App extends Component {
 
     }
 
+    refresh_balance = () => {
+        this.ft_get();
+    }
+
+    ft_transfer = () => {
+        this.setState({ BalanceisLoading: true })
+        TransferTokens(this.state.username, this.state.transferto, this.state.transferamount)
+        .then(json => {
+
+            console.log("DEBUG: jsontransfer :" + json);
+            console.log("DEBUG: jsontransferst :" + json.result);
+            this.refresh_balance();
+            //balance: this.ft_balanceOfSafe(json),
+
+    }
+    )
+            .catch(error => console.error(error))
+
+        //this.setState({ isLoading: false })
+        //this.setState({ name: k })
+
+    }
+
+
     render() {
         var Balance = '';
         if (this.state.BalanceisLoading) {
             Balance = <ActivityIndicator /> // si requête en cours, on affiche un spinner
         }
-        else 
-        {
-            Balance = 
-                    <Text style={styles.balancevalue}>
-                        {this.state.balance}
-                    </Text>
+        else {
+            Balance =
+                <Text style={styles.balancevalue}>
+                    {this.state.balance}
+                </Text>
         }
 
         return (
             <View style={styles.container}>
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={(username) => this.setState({ username })}
+                    value={this.state.username}
+                />
                 <View style={styles.BalanceContainer}>
                     <Text style={styles.name}>
-                        Balance : 
+                        Balance :
                     </Text>
-
-
-                <View style={styles.BalanceContainer}>
-                    {Balance}
+                    <View style={styles.Balancevalue}>
+                        {Balance}
                     </View>
 
                     <TouchableOpacity // on ajoute un "bouton" qui requête une autre bière aléatoire
                         onPress={this.ft_get}
                         style={styles.button}
                     >
-                        <Text>Actualiser</Text>
+                        <Text>Refresh</Text>
                     </TouchableOpacity>
                 </View>
+
+                <View style={styles.SendContainer}>
+                    <Text style={styles.name}>
+                        Transfer tokens :
+                    </Text>
+
+
+                <View style={styles.SideBySide}>
+                    <Text style={styles.name}>
+                        Amount :
+                    </Text>
+                    <TextInput
+                        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                        onChangeText={(transferamount) => this.setState({ transferamount })}
+                        value={this.state.transferamount}
+                    />
+
+                </View>
+
+                <View style={styles.SideBySide}>
+                    <Text style={styles.name}>
+                        To :
+                    </Text>
+                    <TextInput
+                        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                        onChangeText={(transferto) => this.setState({ transferto })}
+                        value={this.state.transferto}
+                    />
+
+
+                </View>
+
+                    <TouchableOpacity // on ajoute un "bouton" qui requête une autre bière aléatoire
+                        onPress={this.ft_transfer}
+                        style={styles.buttonsend}
+                    >
+                        <Text>Send</Text>
+                    </TouchableOpacity>
+                </View>
+
 
             </View>
         )
@@ -131,17 +232,29 @@ const styles = StyleSheet.create({
         flex: 1,
         //justifyContent: 'center',
         //alignItems: 'center',
+        flexDirection: 'column',
         backgroundColor: '#F5FCFF',
     },
     // ajout de styles divers
     BalanceContainer: {
-        margin: 15,
-        height : 34,
+        height: 34,
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center',
     },
+    SendContainer: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+
+
+    SideBySide: {
+        height: 34,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
     name: {
         fontSize: 18,
         fontWeight: '700',
@@ -164,7 +277,17 @@ const styles = StyleSheet.create({
         padding: 5,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    buttonsend: {
+        height: 30,
+        borderWidth: 1,
+        backgroundColor: 'lightblue',
+        borderRadius: 3,
+        padding: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
     }
+
 })
 
 AppRegistry.registerComponent("PcoinWallet", () => App);
