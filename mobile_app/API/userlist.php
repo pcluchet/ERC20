f13dfd682ec26e1246f1a13a04fde3cba172c706
jle-quel <jle-quel@student.42.us.org>
2018-07-12 12:29:51 +0200

    [+]{*}

--- mobile_app/API/userlist.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo '['.
-'{"username" : "Alice", "pubkey" : "ABC123"},'.
-'{"username" : "Bob", "pubkey" : "DEF123"},'.
-'{"username" : "Kevin", "pubkey" : "GHI123"}'.
-']';
-?>
---
+++ network/script/init.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+C_RED="\033[31;01m"
+C_GREEN="\033[32;01m"
+C_YELLOW="\033[33;01m"
+C_BLUE="\033[34;01m"
+C_PINK="\033[35;01m"
+C_CYAN="\033[36;01m"
+C_NO="\033[0m"
+
+export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:$PWD/bin:$PATH
+export FABRIC_CFG_PATH="$PWD"
+CHANNEL_NAME=ptwist
+
+################################################################################
+###                                FUNCTIONS                                 ###
+################################################################################
+
+function clean {
+	rm -rf ./config/*
+	rm -rf ./crypto-config/*
+}
+
+function _err {
+	echo "Failed to generate $1..."
+	exit 1
+}
+
+function generate {	
+	cryptogen generate --config=./crypto-config.yaml || _err "crypto material"
+	configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block || _err "orderer genesis block"
+	configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME || "channel configuration transaction" 
+	configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/MEDSOSMSPanchors.tx -channelID $CHANNEL_NAME -asOrg MEDSOSMSP || "anchor peer update for MEDSOSMSP"
+}
+
+################################################################################
+###                                   MAIN                                   ###
+################################################################################
+
+clean
+generate
